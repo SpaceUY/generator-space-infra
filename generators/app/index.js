6b52da8939a30ca648b49bb6ee49c7baab49fffb
Elina Garcia <36629418+iontechnian@users.noreply.github.com>
2023-09-06 17:08:03 -0300

    fixed gitignore file not being included in package install

--- generators/app/index.js
+++ generators/app/index.js
@@ -67,7 +67,7 @@ module.exports = class extends Generator {
 
   writing() {
     this.fs.copy(
-      this.templatePath(".gitignore"),
+      this.templatePath("gitignore.txt"),
       this.destinationPath(".gitignore")
     );
     this.fs.copyTpl(
---
+++ generators/app/templates/terraform.tfvars
@@ -0,0 +1,2 @@
+project = "<%= project %>"
+region = "<%= region %>"
